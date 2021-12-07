e81fe1b48a7b9f6ddd4403e626acc480ee3de08c
Hamza <Hamzanur2010@hotmail.com>
2021-12-07 22:28:53 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -35,3 +43,7 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2, 8);
+};
